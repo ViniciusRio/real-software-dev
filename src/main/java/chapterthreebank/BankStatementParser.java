d76be8dbfa1c2cc1aaf84ee9f471b405fbd65397
vlmdr <vinicius.rio@edge.ufal.br>
2024-12-24 13:15:20 -0300

    feat(bank): Started new chapter for extension

--- src/main/java/chapterthreebank/BankStatementParser.java
+++
@@ -1,8 +0,0 @@
-package chaptertwobank;
-
-import java.util.List;
-
-public interface BankStatementParser {
-    BankTransaction parseFrom(String line);
-    List<BankTransaction> parseLinesFrom(List<String> lines);
-}
